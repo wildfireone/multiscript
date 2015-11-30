fcf61d64878fa6b00f7e6be6b504b0f3363c3756
wildfireone <wildfireone@gmail.com>
2015-11-30 21:06:00 +0000

    Update datacollect.py

--- datacollect.py
+++ datacollect.py
@@ -8,14 +8,14 @@ while True:
   #get current cpu percentage
   currentcpu = psutil.cpu_percent(interval=1)
   #get current time
-  time = datetime.datetime.now().time()
+  currenttime = datetime.datetime.now().time()
 
   #create the connection to the database
   conn = sqlite3.connect('testdata.db')
   print "Opened database successfully"
 
   #add the values into the database, use str() to turn the values into strings
-  conn.execute("INSERT INTO CPU (TIME,CPUUSE) VALUES ('"+str(time)+"','"+str(currentcpu)+"')")
+  conn.execute("INSERT INTO CPU (TIME,CPUUSE) VALUES ('"+str(currenttime)+"','"+str(currentcpu)+"')")
 
   conn.commit()
   print "Records created successfully";
