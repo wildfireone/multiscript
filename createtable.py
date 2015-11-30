bbd7c54f2d50693bd73d989492bd956ce8dd8a38
wildfireone <wildfireone@gmail.com>
2015-11-30 21:03:42 +0000

    Update createtable.py

--- createtable.py
+++ createtable.py
@@ -1,1 +1,10 @@
-//
+import sqlite3
+
+conn = sqlite3.connect('testdata.db')
+print "Opened database successfully"
+
+conn.execute('''CREATE TABLE CPU (TIME TEXT, CPUUSE TEXT);''')
+print "Table created successfully"
+
+conn.close()
+
