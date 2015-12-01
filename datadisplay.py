b293a8a19b55cec2f32c8a124b3e8872c6e00bd2
wildfireone <wildfireone@gmail.com>
2015-12-01 09:17:28 +0000

    Update datadisplay.py

--- datadisplay.py
+++ datadisplay.py
@@ -19,28 +19,36 @@ while True:
     http_status = "HTTP/1.1 200 OK \n"
     http_type = "Content-Type: text/html\n"
     
+    #create a new table with a title row
     cputable = HTML.Table(header_row=['TIME', 'CPUUSE'])
+    
     #open database
     conn = sqlite3.connect('testdata.db')
     #get data from database
     cursor = conn.execute("SELECT TIME, CPUUSE FROM CPU")
+    #for every row in our database
     for row in cursor:
+      #add a new row to our table
       cputable.rows.append([row[0], row[1]])
     
+    #close the connection
     conn.close()
     
+    #now generate the HTML output
+    #the meta tag here forces a page refresh content = 10 means this happens every 10 seconds
     http_body = """
 	  <!doctype html>
 	  <html>
 	  <head>
 	  	<title> my server data </title>
-	  	<meta http-equiv="refresh" content="5" >
+	  	<meta http-equiv="refresh" content="10" >
 	  </head>
 	  <body> 
 	    """ + str(cputable) + """
 	  </body>
 	  </html>"""
 
+    #finally send everything
     client_connection.send(http_status)
     client_connection.send(http_type)
     client_connection.send(http_body)
